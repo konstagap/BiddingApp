9248a1c6613134a4685db47b03bb6e5caafc20e1
konstantin <konstantyn_agapov@intuit.com>
2023-10-15 18:18:52 -0700

    fixing polling

--- components/hooks/useLatestBid.tsx
+++ components/hooks/useLatestBid.tsx
@@ -3,6 +3,7 @@ import useSWR from 'swr'
 
 export function useLatestBid(jobId: string, isExpired: boolean) {
   const params = new URLSearchParams()
+  params.append('jobId', jobId)
 
   const options = isExpired ? {} : { refreshInterval: 500 }
 
